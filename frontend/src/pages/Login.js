487d93f13c693e2a22d7b0a4c46c8d86682a0165
Aryan Singh <aaryansinghh.2020@gmail.com>
2025-05-25 12:31:58 +0530

    Changes

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -10,7 +10,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+      const res = await axios.post('https://poll-app-03ms.onrender.com/api/auth/login', { email, password });
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('role', res.data.role);
       navigate('/dashboard');
